16a8b0750b55e8160f12e4b761723cc60e226dcc
Bruno Augusto <bruno.augusto.souza@outlook.com>
2026-02-13 12:58:55 -0300

    moved files

--- athena.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_athena_workgroup" "job_analytics_wg" {
-  name = "job-analytics-wg"
-
-  configuration {
-    result_configuration {
-      output_location = "s3://${aws_s3_bucket.job_analytics.bucket}/athena-results/"
-    }
-  }
-}
--- glue.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_glue_catalog_database" "job_analytics_db" {
-  name = "job_analytics"
-}
