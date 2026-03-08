bab4f97f8586efb9f9857120a4bcc883daffd5a9
Bruno Augusto <bruno.augusto.souza@outlook.com>
2026-03-08 12:27:43 -0300

    new main based on most recent langchain version

--- ai-orchestrated-big-data-analytics-aws/ai_data_analytics_mcp/main.py
+++ ai-orchestrated-big-data-analytics-aws/ai_data_analytics_mcp/main.py
@@ -1,94 +1,51 @@
 import json
 from dotenv import load_dotenv
 
-from langchain_openai import ChatOpenAI
-from langchain_core.tools import tool
-from langgraph.prebuilt import create_react_agent
-
-from glue_metadata import get_glue_metadata
-from athena_tools import execute_query
-from sql_generator import build_ctas_query
-from utils import load_prompt
-from config import PROMPT_SQL_PATH
+from src.suggest_queries import suggest_queries
+from src.athena_executor import execute_query
+from src.utils import extract_external_location
+from src.bucket_cleanse import delete_files
 
 load_dotenv()
-
-llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    temperature=0
-)
-
-
-@tool
-def list_tables() -> str:
-    """Return Glue tables and columns."""
-
-    tables = get_glue_metadata()
-
-    return json.dumps(tables)
-
-
-@tool
-def generate_sql(user_request: str) -> str:
-    """Generate SQL query for Athena."""
-
-    tables = get_glue_metadata()
-
-    prompt = load_prompt(
-        PROMPT_SQL_PATH,
-        tables=tables,
-        user_request=user_request
-    )
-
-    response = llm.invoke(prompt)
-
-    return response.content
-
-
-@tool
-def run_analytics(sql_query: str) -> str:
-    """Execute CTAS query and store result in S3 Gold layer."""
-
-    dataset_name = "ai_generated_dataset"
-
-    ctas = build_ctas_query(
-        sql_query,
-        dataset_name
-    )
-
-    query_id = execute_query(ctas)
-
-    return f"Query executed successfully. QueryId={query_id}"
-
-
-tools = [
-    list_tables,
-    generate_sql,
-    run_analytics
-]
-
-
-agent = create_react_agent(
-    model=llm,
-    tools=tools
-)
 
 
 def run():
 
     print("\nAI Data Analytics MCP\n")
 
-    user_input = input("Ask your question:\n")
+    suggestions = suggest_queries()
 
-    result = agent.invoke(
-        {
-            "messages": [
-                ("user", user_input)
-            ]
-        }
+    print("\nAvailable analyses:\n")
+
+    for item in suggestions:
+        print(f"{item['id']} - {item['description']}")
+
+    choice = int(input("\nChoose query number: "))
+
+    selected = next(
+        q for q in suggestions
+        if q["id"] == choice
     )
 
-    print(result)
+    sql = selected["sql"]
+
+    ctas = sql
+    result_path = extract_external_location(ctas)
+    relative_path = "gold/" + result_path.split("gold/")[-1]
+    delete_files(result_path)
+
+    query_id = execute_query(ctas)
+    
+    print("\nQuery executed!\n")
+
+    print("SQL:")
+    print(sql)
+
+    print("\nS3 location:")
+    print(result_path)
+
+    print("\nAthena Query ID:")
+    print(query_id)
 
 
 if __name__ == "__main__":
