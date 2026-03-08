7f999923b34ff2f64f56f15f30d4686777e930e8
Bruno Augusto <bruno.augusto.souza@outlook.com>
2026-03-08 12:31:01 -0300

    removed prompt sql

--- ai-orchestrated-big-data-analytics-aws/ai_data_analytics_mcp/src/config.py
+++ ai-orchestrated-big-data-analytics-aws/ai_data_analytics_mcp/src/config.py
@@ -9,6 +9,5 @@ DATABASE_NAME = "nyc_hvfhs_analytics_lakehouse"
 ATHENA_OUTPUT = f"s3://{BUCKET_NAME}/athena-results/"
 GOLD_PATH = f"s3://{BUCKET_NAME}/gold"
 REGION = os.getenv("AWS_REGION")
-PROMPT_SQL_PATH = "prompts/generate_sql_prompt.txt"
 PROMPT_SUGGEST_PATH = "prompts/suggested_queries.txt"
 OPEN_API_KEY = os.getenv("OPENAI_API_KEY")
